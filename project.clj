bca3aab47bf5e554856a136cd376067069be280b
Chris Burgin <chrisburgin95@gmail.com>
2018-08-25 16:42:37 -0400

    Version Number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marker "1.0.0"
+(defproject marker "1.0.1"
   :description "A Clojure library that makes it easy to colorize your outputs."
   :url "https://github.com/chrisburgin95/marker"
   :license {:name "MIT"
